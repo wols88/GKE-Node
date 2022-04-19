29e6cd12dd1ef62bebfe2cab59d67a67fcf84d96
wols88 <77319581+wols88@users.noreply.github.com>
2022-04-19 10:58:06 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Wilson Winarto!\n')
+    res.send('Wilson Winarto, 52019038, Teknik Informatika')
 })
 
 app.listen(PORT, HOST)
